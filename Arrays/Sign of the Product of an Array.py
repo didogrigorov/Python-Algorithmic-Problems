31206d7f3a1b02877773f8b4722788fe7ef099da
didogrigorov <dido.grigorov@gmail.com>
2023-06-23 18:36:35 +0300

    2 new array algorithmic problems solved

---
+++ Arrays/Sign of the Product of an Array.py
@@ -0,0 +1,9 @@
+def arraySign(self, nums: List[int]) -> int:
+    negatives = 0
+
+    for n in nums:
+        if n == 0:
+            return 0
+        negatives += (1 if n < 0 else 0)
+
+        return -1 if negatives % 2 else 1
