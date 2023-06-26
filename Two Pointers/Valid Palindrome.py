94d52d144cde7e8b14e4ae4d0260a951c664afed
didogrigorov <dido.grigorov@gmail.com>
2023-06-26 15:44:28 +0300

    Re-arraning the directory

---
+++ Two Pointers/Valid Palindrome.py
@@ -0,0 +1,40 @@
+def isPalindrom(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+        if not s[right].isalnum():
+            right -= 1
+
+        if s[left].lower() != s[right].lower():
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+word = "madame"
+print(isPalindrom(word))
+
+# class Solution:
+#     def isPalindrome(self, s):
+#         left = 0
+#         right = len(s) - 1
+#
+#         while left < right:
+#             while left < right and not s[left].isalnum():
+#                 left += 1
+#             while left < right and not s[right].isalnum():
+#                 right -= 1
+#
+#             if s[left].lower() != s[right].lower():
+#                 return False
+#
+#             left += 1
+#             right -= 1
+#
+#         return True
