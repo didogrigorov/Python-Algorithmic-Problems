7346c717a625e263e642990524388135dca49ff8
didogrigorov <dido.grigorov@gmail.com>
2023-07-11 12:58:48 +0300

    2 problems solved from the Sliding window technique.

---
+++ Arrays/Next Greater Element.py
@@ -0,0 +1,23 @@
+from typing import List
+def nextGreaterElement(nums1: List[int], nums2: List[int]) -> List[int]:
+    nums1Idx = { n : i for i, n in enumerate(nums1)}
+    res = [-1] * len(nums1)
+
+    stack = []
+    for i in range(len(nums2)):
+        current = nums2[i]
+
+        while stack and current > stack[-1]:
+            value = stack.pop()
+            idx = nums1Idx[value]
+            res[idx] = current
+
+        if current in nums1Idx:
+            stack.append(current)
+
+    return res
+
+
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
+print(nextGreaterElement(nums1,nums2))
