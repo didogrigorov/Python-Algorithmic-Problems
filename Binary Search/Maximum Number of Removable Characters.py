123c2d3d054c0ce6d67770551dad3bfb82550670
didogrigorov <dido.grigorov@gmail.com>
2023-09-25 19:18:18 +0300

    3 Hard Binary Search Problems Solved

---
+++ Binary Search/Maximum Number of Removable Characters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        def is_subseq(s, subseq):
+            i1, i2 = 0, 0
+
+            while i1 < len(s) and i2 < len(subseq):
+                if i1 in removed or s[i1] != subseq[i2]:
+                    i1 += 1
+                    continue
+                i1 += 1
+                i2 += 1
+
+            return i2 == len(subseq)
+
+        result = 0
+
+        l, r = 0, len(removable) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            removed = set(removable[:m + 1])
+            if is_subseq(s, p):
+                result = max(result, m + 1)
+                l = m + 1
+            else:
+                r = m - 1
+
+        return result
